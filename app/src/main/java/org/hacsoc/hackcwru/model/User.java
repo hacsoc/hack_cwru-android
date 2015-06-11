8b1f04113ac0ba65a6848a099e2bd88e7a0d0f0d
Andrew Mason <mason@case.edu>
2015-06-10 22:04:43 -0700

    add setters for User model and toJSONObject method.

--- app/src/main/java/org/hacsoc/hackcwru/model/User.java
+++ app/src/main/java/org/hacsoc/hackcwru/model/User.java
@@ -1,5 +1,8 @@
 package org.hacsoc.hackcwru.model;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Calendar;
 
 /**
@@ -8,7 +11,7 @@ import java.util.Calendar;
  * Created by andrew on 6/9/15.
  */
 public class User {
-    private int id;
+    private Integer id;
     private String name;
     private String email;
     private String institution;
@@ -20,4 +23,119 @@ public class User {
      */
     private Calendar created_at;
     private Calendar updated_at;
+
+    private String password;
+    private String passwordConfirmation;
+
+    /**
+     * Set the id of this user.
+     *
+     * @param id
+     */
+    public void setID(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * Set the name of this user.
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Set the email of this user.
+     *
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Set the institution of this user.
+     *
+     * @param institution
+     */
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    /**
+     * Set the staff parameter of this user.
+     *
+     * @param staff
+     */
+    public void setStaff(boolean staff) {
+        this.staff = staff;
+    }
+
+    /**
+     * Set the mentor parameter of this user.
+     *
+     * @param mentor
+     */
+    public void setMentor(boolean mentor) {
+        this.mentor = mentor;
+    }
+
+    /**
+     * Set the created_at time of this user.
+     *
+     * @param created_at
+     */
+    public void setCreatedAt(Calendar created_at) {
+        this.created_at = created_at;
+    }
+
+    /**
+     * Set the updated_at time of this user.
+     *
+     * @param updated_at
+     */
+    public void setUpdatedAt(Calendar updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    /**
+     * Set the password of this user.
+     *
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Set the password confirmation of this user.
+     *
+     * @param passwordConfirmation
+     */
+    public void setPasswordConfirmation(String passwordConfirmation) {
+        this.passwordConfirmation = passwordConfirmation;
+    }
+
+    /**
+     * Convert this user to a JSONObject for serialization.
+     * TODO: add more of the fields to this, or figure out a better way to do it.
+     *
+     * @return
+     */
+    public JSONObject toJSONObject() {
+        JSONObject json = new JSONObject();
+        try {
+            if (id != null) {
+                json.put("id", id);
+            }
+        } catch (JSONException e) {}
+        try {
+            if (name != null) {
+                json.put("name", name);
+            }
+        } catch (JSONException e) {}
+
+        return json;
+    }
 }
